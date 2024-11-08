94ba7612cd27e223f4bb381b8520319aaf55e53f
reis071 <reiscorreia071@gmail.com>
2024-11-07 23:42:36 -0300

    add usuario padrao com userDetailService

--- src/main/java/com/example/api_application/security/SecurityConfig.java
+++ src/main/java/com/example/api_application/security/SecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -21,13 +24,12 @@ public class SecurityConfig {
                 .requestMatchers("grupo/cadastrar").authenticated()
         );
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("teste").password("123").roles("USER");
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager( User.withUsername("user").password("{noop}123").roles("USER").build() );
     }
 }
