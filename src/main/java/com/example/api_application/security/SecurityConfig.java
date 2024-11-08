7221a2faf3f36c7de243879c9c607f5961570bce
reis071 <reiscorreia071@gmail.com>
2024-11-07 23:55:09 -0300

    add passworEncoder para criptografia de senhas

--- src/main/java/com/example/api_application/security/SecurityConfig.java
+++ src/main/java/com/example/api_application/security/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -30,6 +32,12 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager( User.withUsername("user").password("{noop}123").roles("USER").build() );
+        return new InMemoryUserDetailsManager( User.withUsername("user")
+                .password("{bcrypt}$2a$12$rOtqfUaSY.XEk6OzuldWzumaX/2ZShnzp8exiLKF84SvbPVxPK91q").roles("USER").build() );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return  PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
